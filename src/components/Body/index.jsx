c6dc980c7e0fa71712c984f048d000af6d0f65cb
unknown <ray_ramez@yahoo.com>
2019-03-09 11:18:52 -0800

    added more photos

--- src/components/Body/index.jsx
+++ src/components/Body/index.jsx
@@ -4,11 +4,18 @@ import bill from "./images/bill.jpg";
 import obama from "./images/obama.jpg";
 import reagan from "./images/reagan.jpg";
 import trump from "./images/trump.jpg";
+import wa from "./images/wa.jpg";
+import richard from "./images/richard.jpg";
+import james from "./images/james.jpg";
+import kennedy from "./images/kennedy.jpg";
+import bushFather from "./images/bushFather.jpg";
+import bushSon from "./images/bushSon.jpg";
+
 let shuffle = require("shuffle-array");
 
 class Body extends Component {
   state = {
-    images: [bill, obama, reagan, trump],
+    images: [bill, obama, reagan, trump, wa,richard, james, bushFather,kennedy, bushSon],
     pickedImageArray: []
   };
   render() {
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,10 +4,10 @@ import "./header.css";
 function Header() {
   return (
     <div className="jumbotron text-center header">
-      <h1 className="display-3 font-weight-bold">Clicky Game!</h1>
-      <h2 className="my-2">
+      <h2 className="display-5 font-weight-bold">Clicky Game!</h2>
+      <h5 className="my-2">
         Click on an image to earn points, but don't click on any more than once!
-      </h2>
+      </h5>
     </div>
   );
 }
